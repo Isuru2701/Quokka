e0f82a631e8dde4cd8afab36d2c266e7d7f79450
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-09 10:10:17 +0530

    seperated iv dv

--- NeuralNetworkTrainer.py
+++ NeuralNetworkTrainer.py
@@ -3,4 +3,13 @@ import tensorflow as tf
 
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.model_selection import train_test_split
 
+# Build dataframe
+df = pd.read_csv('datasets/forestfires.csv')
+df = df.drop(['X', 'Y', 'day'], axis=1)
+
+# Define independent and dependent variables
+X = df['month', 'FFMC', 'DMC', 'DC', 'ISI', 'temp', 'RH', 'wind', 'rain']
+Y = (df['area'] > 0).astype(int)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
