2510b9d628c2929a0f67a049d636da657972bac3
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-09 14:38:54 +0530

    first model complete, needs refinement

--- NeuralNetworkTrainer.py
+++ NeuralNetworkTrainer.py
@@ -10,20 +10,42 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('datasets/forestfires.csv')
 df.drop(['X', 'Y', 'day', 'FFMC', 'DMC', 'DC', 'ISI'], axis=1, inplace=True)
 df.dropna(inplace=True)
-print(df.head())
+
+# Dictionary mapping month names to month numbers
+# needed this outside there's an error with the df
+month_to_number = {
+    'jan': 1,
+    'feb': 2,
+    'mar': 3,
+    'apr': 4,
+    'may': 5,
+    'jun': 6,
+    'jul': 7,
+    'aug': 8,
+    'sep': 9,
+    'oct': 10,
+    'nov': 11,
+    'dec': 12
+}
+
+# Convert month names to month numbers using map() function
+df['month'] = df['month'].map(month_to_number)
+
 
 # Define independent and dependent variables
 X = df[['month', 'temp', 'RH', 'wind', 'rain']]
 Y = pd.DataFrame((df['area']>0).astype(float))
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
+print(X_train.dtypes, "|||", Y_train.dtypes)
 
-print(type(X_train), type(Y_train))
+
+print(type(X_train), "||||", type(Y_train))
 
 #init model rn
 model = Sequential(
     [
-        Dense(64, activation='relu', input_shape=(5,)),
+        Dense(64, activation='relu', input_shape=(1,)),
         Dense(32, activation='relu'),
         Dense(1, activation='sigmoid')
     ]
