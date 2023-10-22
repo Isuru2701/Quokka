a84fa3abffd60f0844e1fdceaa69f7d5761e8396
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-22 13:55:22 +0530

    working with QTrainer.py

---
+++ tools/QTrainer.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import tensorflow as tf
+
+# using keras
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+
+"""
+    Steps
+    1. Load forestfires.csv
+    2. Drop area column
+    3. Add new column 'class' and fill with 1 (fire)
+    4. Load new dataset for days without fire and add new column 'class' with values 0 (no_fire)
+    5. merge two datasets
+    6. split into testing and training
+    7. build model
+    8. compile model
+    9. train model
+    10. test & evaluate
+
+"""
+
+# load forestfires.csv
+
+df = pd.read_csv('../datasets/forestfires.csv')
